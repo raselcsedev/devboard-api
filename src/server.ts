66572dc13d342efcedc0685ba11f7ecfce47027f
Rasel Aktar <rasel.aktar@disystem.net>
2026-06-27 12:06:51 +0600

    sever.ts update

--- src/server.ts
+++ src/server.ts
@@ -38,5 +38,23 @@ import app from "./app.js";
 import { env } from "./config/env.js";
 
 app.listen(env.port, () => {
-  console.log(`${env.appName} running on port ${env.port}`);
+  console.log("=================================");
+  console.log(`🚀 ${env.appName} started`);
+  console.log(`🌍 Environment : ${env.nodeEnv}`);
+  console.log(`📦 API Version : ${env.apiVersion}`);
+  console.log(`🔗 Port        : ${env.port}`);
+  console.log("=================================");
+});
+
+const server = app.listen(env.port, () => {
+  console.log(`🚀 ${env.appName} running on port ${env.port}`);
+});
+
+process.on("SIGTERM", () => {
+  console.log("SIGTERM received. Shutting down gracefully...");
+
+  server.close(() => {
+    console.log("Server closed.");
+    process.exit(0);
+  });
 });
