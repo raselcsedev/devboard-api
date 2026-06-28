375d4adb75807f56dedf34dda8b2985560f39e55
raselcsedev <raselcse.dev@gmail.com>
2026-06-28 23:26:01 +0600

    midleware add

--- src/app.ts
+++ src/app.ts
@@ -3,31 +3,44 @@ import { env } from "./config/env.js";
 
 const app = express();
 
-// Built-in middleware
-app.use(express.json());
+// // Built-in middleware
+// app.use(express.json());
 
-app.get("/", (_req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Welcome to DevBoard API",
-  });
-});
+// app.get("/", (_req, res) => {
+//   res.status(200).json({
+//     success: true,
+//     message: "Welcome to DevBoard API",
+//   });
+// });
 
-app.get("/health", (_req, res) => {
-  res.status(200).json({
-    success: true,
-    status: "OK",
-    uptime: process.uptime(),
-    timestamp: new Date().toISOString(),
-  });
-});
+// app.get("/health", (_req, res) => {
+//   res.status(200).json({
+//     success: true,
+//     status: "OK",
+//     uptime: process.uptime(),
+//     timestamp: new Date().toISOString(),
+//   });
+// });
 
-app.get("/api/info", (_req, res) => {
-  res.status(200).json({
-    name: env.appName,
-    version: env.apiVersion,
-    environment: env.nodeEnv,
-  });
-});
+// app.get("/api/info", (_req, res) => {
+//   res.status(200).json({
+//     name: env.appName,
+//     version: env.apiVersion,
+//     environment: env.nodeEnv,
+//   });
+// });
 
-export default app;
+// app.use((req, _res, next) => {
+//   console.log("========== REQUEST ==========");
+//   console.log("Method :", req.method);
+//   console.log("URL    :", req.originalUrl);
+//   console.log("Time   :", new Date().toISOString());
+
+//   next();
+// });
+
+// export default app;
+
+import { loggerMiddleware } from "./middleware/logger.middleware.js";
+
+app.use(loggerMiddleware);
